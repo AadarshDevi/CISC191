ad9384d80979795c3d571b9fd80e4968cdce6b4d
Aadarsh Devi <99939931+AadarshDevi@users.noreply.github.com>
2025-10-26 17:20:25 -0700

    Add files via upload

---
+++ Week_9/PalindromeChecker.java
@@ -0,0 +1,82 @@
+package week_9;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    private static Scanner scanner;
+
+    public static void main(String[] args) {
+
+        /*
+        This is created here so I do not have to create it many times again and again.
+         */
+        scanner = new Scanner(System.in); // user input
+
+        boolean running = true; // boolean to run while loop
+
+        // feedback loop
+        while (running) {
+
+            // get word
+            String value = getInput();
+
+            // check if word is
+            if (value.equalsIgnoreCase("--exit")) {
+                System.out.println("Exiting Palindrome Checker...");
+                scanner.close();
+                running = false;
+                continue; // starts next iteration and exits since condition met
+            }
+
+            boolean isPalindrome = checkPalindrome(value);
+            if (isPalindrome) System.out.println("Yes, " + value + " is a palindrome.");
+            else System.out.println("No, \"" + value + "\" is not a palindrome.");
+        }
+
+
+    }
+
+    private static boolean checkPalindrome(String value) {
+
+        if (value.isBlank()) return false;
+
+        /*
+         * Takes value, converts it into a List<String> and uses it as a parameter to Deque<String>
+         * -> String
+         * -> String[] = String.split("")
+         * -> List<String> = Array.asList(String[])
+         * -> Deque<String> = new ArrayDeque<String>(List<String>)
+         */
+        Deque<String> characterizedValue = new ArrayDeque<>(Arrays.asList(value.split("")));
+        int dequeLength = characterizedValue.size();
+
+        for (
+                int startPointer = 0, endPointer = dequeLength - 1; startPointer <= dequeLength + 1 && startPointer <= endPointer; startPointer++, endPointer--) {
+//            System.out.println("SI: " + startPointer + ", EP: " + endPointer);
+            if (startPointer == endPointer) return true;
+            String startLetter = characterizedValue.getFirst();
+            String endLetter = characterizedValue.getLast();
+//            System.out.println("F: " + startLetter + ", L: " + endLetter);
+            if (!startLetter.equalsIgnoreCase(endLetter)) return false;
+            characterizedValue.pollFirst();
+            characterizedValue.pollLast();
+        }
+
+        return true;
+    }
+
+    private static String getInput() {
+        boolean invalidInput = true;
+        String value = null;
+        while (invalidInput) {
+            System.out.print("Enter a word (\"--exit\" to exit): ");
+            value = scanner.nextLine();
+            if (!value.isBlank()) invalidInput = false;
+            else System.err.println("Please Enter a word");
+        }
+        return value.toLowerCase();
+    }
+}
