ce29a987f20d3ade090e33af4c742e727cb1864d
Aadarsh Devi <99939931+AadarshDevi@users.noreply.github.com>
2025-11-08 19:50:20 -0800

    Update and rename Main.java to HourlyWagesMain.java

--- Week10_11/ActionListeners/HourlyWagesMain.java
+++ Week10_11/ActionListeners/HourlyWagesMain.java
@@ -2,7 +2,7 @@ package week_10_11.lab1;
 
 import javax.swing.*;
 
-public class Main {
+public class HourlyWagesMain {
     public static void main(String[] args) {
         HourlyWageUI hwUI = new HourlyWageUI("Hourly Wage", JFrame.EXIT_ON_CLOSE);
         hwUI.showGUI();
